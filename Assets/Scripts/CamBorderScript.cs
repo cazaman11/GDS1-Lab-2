307de33cca1b277aaa36fd95d02ca9892e8a3f20
cazaman11 <38904836+cazaman11@users.noreply.github.com>
2019-03-21 10:15:36 +1100

    Fixed Camera
    Added functionality for the camera to follow mario and to only go to the right. It also has 2 barriers to block mario and to despawn objects offscreen.

---
+++ Assets/Scripts/CamBorderScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBorderScript : MonoBehaviour {
+    Collider thisCollider;
+    Rigidbody thisRigidbody;
+    [SerializeField]
+    bool left;
+	// Use this for initialization
+	void Start () {
+        thisRigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        
+	}  
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name != "Mario" && left)
+        {
+            collision.gameObject.SetActive(false);
+        }
+        if (collision.gameObject.name != "Mario" && !left)
+        {
+            collision.gameObject.SetActive(true);
+        }
+        if (collision.gameObject.name == "Mario")
+        {
+            //thisRigidbody.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Mario")
+        {
+            /*
+            thisRigidbody.constraints = RigidbodyConstraints.None;
+            thisRigidbody.constraints = RigidbodyConstraints.FreezeRotation;
+            thisRigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+            thisRigidbody.constraints = RigidbodyConstraints.FreezePositionZ;
+            */
+
+        }
+    }
+}
