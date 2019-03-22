2db855687a6012eab5535932fc3e6ab352090d5e
cazaman11 <38904836+cazaman11@users.noreply.github.com>
2019-03-22 12:00:39 +1100

    Added lives and death
    Functionality that reloads the scene when the player dies and game over's while keeping lives tracked and setup for persistant objects. Lives can be increased and decreased for 1 Ups and 100 coins.

---
+++ Assets/Scripts/LivesScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LivesScript : MonoBehaviour {
+
+    public int lives = 3;
+    int coinTotal; //Make this link to coin script
+    bool checkpointPassed = false; //Add checkpoint object to flag if passed
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(lives == 0)
+        {
+            GameOver();
+        }
+        if(coinTotal == 100)
+        {
+            coinTotal = 0;
+            OneUp();
+        }
+        if(lives > 100)
+        {
+            lives = 100;
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            GameOver();
+        }
+	}
+
+    public void OneUp()
+    {
+        lives += 1;
+    }
+
+    public void Died()
+    {
+        lives -= 1;
+        // Move mario to world spawn or checkpoint
+        // Move camera there
+        // Might make separate scenes for easy loading of objects
+        SceneManager.LoadScene("GameScene");
+    }
+
+    void GameOver()
+    {
+        SceneManager.LoadScene("GameScene");
+        lives = 3;
+    }
+}
