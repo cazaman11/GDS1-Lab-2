474ccd9dd972f8dc2db5d842adf13a09b32c4f5f
AlexanderLeydon <37314497+AlexanderLeydon@users.noreply.github.com>
2019-03-22 14:05:15 +1100

    Made Goomba

---
+++ Assets/Scripts/GoombaMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoombaMovement : MonoBehaviour {
+
+    private bool goLeft;
+    [SerializeField]
+    private float speed;
+
+	// Use this for initialization
+	void Awake () {
+        goLeft = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (goLeft)
+        {
+            transform.position += Vector3.left * speed;
+        }
+        else {
+            transform.position += Vector3.right * speed;
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag != "Floor") {
+            goLeft = !goLeft;
+        }
+    }
+}
